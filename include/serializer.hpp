f5eb626ddcffbbdb430d274597376f73d39f8930
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-06-27 21:04:08 -0400

    some format fixes

--- include/serializer.hpp
+++ include/serializer.hpp
@@ -37,6 +37,7 @@ namespace circus
         void handle(Arg &&arg)
         {
             using value_type = decltype(arg.second);
+            stream << std::endl;
             stream << tokens__::to_literal(tokens__::TYPE::TK_DOLLA) << arg.first << tokens__::to_literal(tokens__::TYPE::TK_COLON);
             if constexpr (std::is_class_v<std::remove_cvref_t<value_type>> && !traits::StreamableVector<value_type> && !traits::StringLike<value_type>)
             {
@@ -83,7 +84,8 @@ namespace circus
         template <std::size_t... Is, typename... Args>
         void handler(std::index_sequence<Is...>, Args &&...args)
         {
-            (handle<Is, sizeof...(Args)>(make_pair_serializable<Is>(std::forward<Args>(args))), ...);
+            constexpr std::size_t MAX = sizeof...(Args);
+            (handle<Is, MAX>(make_pair_serializable<Is>(std::forward<Args>(args))), ...);
         }
 
     public:
@@ -93,13 +95,15 @@ namespace circus
         template <typename... Args>
         void operator()(Args &&...args) &
         {
-            handler(std::make_index_sequence<sizeof...(Args)>{}, std::forward<Args>(args)...);
+            constexpr std::size_t MAX = sizeof...(Args);
+            handler(std::make_index_sequence<MAX>{}, std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         serializer &operator<<(Args &&...args)
         {
-            (handle(std::forward<Args>(args)), ...);
+            constexpr std::size_t MAX = sizeof...(Args);
+            handler(std::make_index_sequence<MAX>{}, std::forward<Args>(args)...);
             return *this;
         }
 
