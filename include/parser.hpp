fe7d4c56dd4bcdeaebec65db1cde989a103a9a12
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-06-29 09:55:10 -0400

    added some more parser functionality

--- include/parser.hpp
+++ include/parser.hpp
@@ -11,6 +11,21 @@ namespace circus
         std::vector<tokens__> _in;
         std::size_t _curs;
 
+        [[nodiscard]] tokens__ f_peek_at(const std::size_t off) const noexcept
+        {
+            return _in[_curs + off];
+        };
+
+        [[nodiscard]] tokens__ f_previous() const noexcept
+        {
+            _in[_curs - 1];
+        };
+
+        [[nodiscard]] tokens__ f_peek_next() const noexcept
+        {
+            return _in[_curs + 1];
+        };
+
         [[nodiscard]] tokens__ f_peek() const noexcept
         {
             return _in[_curs];
